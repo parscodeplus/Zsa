c81475d6b071b5aee1da316a21280e7b2dd7f7cf
ParsCodePlus <parscodeplus@gmail.com>
2024-06-28 16:07:28 +0000

    infinity scroll update

--- components/framer-buttom-navigation.tsx
+++ components/framer-buttom-navigation.tsx
@@ -22,15 +22,15 @@ function BottomNavigation() {
   let [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
-    <div className='fixed bottom-0 left-0 z-50 flex h-16 w-full items-center justify-center bg-transparent'>
-      <div className='z-0 flex w-full items-center justify-center space-x-2 rounded-t-2xl bg-primary p-4 dark:bg-background'>
+    <div className='fixed bottom-0 left-0 z-50 flex h-14 w-full items-center justify-center bg-transparent'>
+      <div className='z-0 flex w-full items-center justify-center space-x-1  rounded-t-2xl bg-primary p-4 dark:bg-background'>
         {tabs.map((tab) => (
           <div
             key={tab.id}
             onClick={() => setActiveTab(tab.id)}
             className={`${
               activeTab === tab.id ? 'relative' : 'hover:text-white/60'
-            } relative rounded-full px-3 py-1.5 text-sm font-medium text-foreground outline-sky-400 transition focus-visible:outline-2`}
+            } relative rounded-full px-2 py-1.5 text-sm font-medium text-foreground outline-sky-400 transition focus-visible:outline-2`}
             style={{
               WebkitTapHighlightColor: 'transparent',
             }}
@@ -45,16 +45,16 @@ function BottomNavigation() {
             )}
             <div className='flex flex-col items-center justify-center'>
               <button
-                className={`transition-top relative flex h-10 w-10 items-center justify-center rounded-full bg-blue-600 text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] duration-300 hover:bg-blue-700 ${
+                className={`transition-top relative flex h-11 w-11 items-center justify-center rounded-full bg-blue-600 text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] duration-300 hover:bg-blue-700 ${
                   activeTab === tab.id
-                    ? '-top-1 animate-bounce duration-1000'
+                    ? '-top-1 '
                     : 'top-0 mix-blend-normal'
                 }`}
               >
                 <tab.icon
-                  className={`transition-top relative h-6 w-6 duration-300 ${
+                  className={`transition-top relative h-6 w-6 duration-1000 ${
                     activeTab === tab.id
-                      ? '-top-1 animate-bounce text-white duration-1000'
+                      ? '-top-1  text-white '
                       : 'top-0 text-gray-400 mix-blend-normal'
                   }`}
                 />
